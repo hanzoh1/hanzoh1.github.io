9fcfaea115419a10b3c99e795e4aa9836c77102b
hanzoh <hattorimayato@gmail.com>
2019-11-27 19:24:58 +0900

    java script function added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Undo";
+    moreText.style.display = "inline";
+  }
+}
